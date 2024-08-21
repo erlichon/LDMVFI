406036e15345c608531bfb4f88b8a871223b60e1
= <itayerlich96@gmail.com>
2024-08-21 21:31:38 +0300

    adding rand_rotation vit transformation

--- ldm/data/vfitransforms.py
+++ ldm/data/vfitransforms.py
@@ -5,6 +5,10 @@ import torchvision.transforms.functional as TF
 
 
 def rand_crop(*args, sz):
+    # import pdb; pdb.set_trace()
+    # if args[0].size == (1024, 1024):
+    #     for i in range(len(args)):
+    #         args[i] = TF.resize(args[i], (512, 512))
     i, j, h, w = torchvision.transforms.RandomCrop.get_params(args[0], output_size=sz)
     out = []
     for im in args:
@@ -12,7 +16,7 @@ def rand_crop(*args, sz):
     return out
 
 
-def rand_flip(*args, p):
+def rand_flip(*args, p=0.5):
     out = list(args)
     if random.random() < p:
         for i, im in enumerate(out):
@@ -22,6 +26,25 @@ def rand_flip(*args, p):
             out[i] = TF.vflip(im)
     return out
 
+def rand_rotation(*args, p=0.5, max_degrees_per_step=3):
+    out = list(args)
+    rot_deg = random.uniform(-max_degrees_per_step, max_degrees_per_step)
+    if random.random() < p:
+        if random.random() < 0.5:
+            for i, im in enumerate(out):
+                out[i] = TF.rotate(im, rot_deg)
+        else:
+            for i, im in enumerate(out):
+                out[i] = TF.rotate(im, i*rot_deg)
+    return out
+
+# def rand_continuous_translation(*args, p, max_step_size):
+#     out = list(args)
+#     rot_deg = random.randint(-max_degrees_per_step, max_degrees_per_step)
+#     if random.random() < p:
+#         for i, im in enumerate(out):
+#             out[i] = TF.rotate(im, i*rot_deg)
+#     return out
 
 def rand_reverse(*args, p):
     if random.random() < p:
